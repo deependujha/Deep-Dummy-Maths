d001cb08599e6bdf81e9ed9a681b3fca83eb27a4
deependujha <deependujha21@gmail.com>
2024-06-25 13:27:28 +0530

    support for factorial added

--- src/deep_dummy_maths/my_maths.py
+++ src/deep_dummy_maths/my_maths.py
@@ -16,3 +16,10 @@ def my_pro(a: int, b: int) -> int:
 def my_div(a: int, b: int) -> float:
     print("Deep dummy maths 🚀")
     return a/b
+
+def my_factorial(n: int) -> int:
+    print("Deep dummy maths 🚀")
+    if n == 0:
+        return 1
+    else:
+        return n * my_factorial(n-1)
--- tests/test_maths.py
+++ tests/test_maths.py
@@ -1,4 +1,4 @@
-from deep_dummy_maths import my_sum, my_diff, my_pro, my_div
+from deep_dummy_maths import my_sum, my_diff, my_pro, my_div, my_factorial
 
 def test_my_sum():
     assert my_sum(1,2) == 3
@@ -11,3 +11,6 @@ def test_my_pro():
 
 def test_my_div():
     assert my_div(1,2) == 0.5
+
+def test_my_factorial():
+    assert my_factorial(5) == 120
